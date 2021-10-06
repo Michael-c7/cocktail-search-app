69b03093b9e1c810242ac1356adbebcb2f738670
Michael Carr <mcarr2468@gmail.com>
2021-10-05 19:01:10 -0500

    V1.1 finished base css styles done

--- src/App.js
+++ src/App.js
@@ -18,18 +18,42 @@ const App = () => {
 
             <section className="search">
                 <h2 class="search__heading">Search Your Favorite Cocktail</h2>
-                <input className="search__input"/>
+                <input className="search__input" placeholder="Eg: A1" autoFocus/>
             </section>
 
             <section className="cocktail">
                 <h2 className="cocktail__heading">Cocktails</h2>
                 <ul className="cocktail__cards">
                     <li className="cocktail__card">
-                        <img src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" alt=""/>
-                        <h2>A1</h2>
-                        <h3>cocktail glass</h3>
-                        <h4>Alcoholic</h4>
-                        <button><a>Details</a></button>
+                        <img className="card__img" src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" alt=""/>
+                        <h2 className="card__name">A1</h2>
+                        <h3 className="card__glass-type">cocktail glass</h3>
+                        <h4 className="card__type">Alcoholic</h4>
+                        <button className="card__details-btn"><a>Details</a></button>
+                    </li>
+
+                    <li className="cocktail__card">
+                        <img className="card__img" src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" alt=""/>
+                        <h2 className="card__name">A1</h2>
+                        <h3 className="card__glass-type">cocktail glass</h3>
+                        <h4 className="card__type">Alcoholic</h4>
+                        <button className="card__details-btn"><a>Details</a></button>
+                    </li>
+
+                    <li className="cocktail__card">
+                        <img className="card__img" src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" alt=""/>
+                        <h2 className="card__name">A1</h2>
+                        <h3 className="card__glass-type">cocktail glass</h3>
+                        <h4 className="card__type">Alcoholic</h4>
+                        <button className="card__details-btn x"><a>Details</a></button>
+                    </li>
+
+                    <li className="cocktail__card">
+                        <img className="card__img" src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" alt=""/>
+                        <h2 className="card__name">A1</h2>
+                        <h3 className="card__glass-type">cocktail glass</h3>
+                        <h4 className="card__type">Alcoholic</h4>
+                        <button className="card__details-btn"><a>Details</a></button>
                     </li>
                 </ul>
             </section>
